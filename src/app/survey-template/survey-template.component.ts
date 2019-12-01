88939259a74bcb613d91f10c21fbdc5cf3ffdc77
Viktor Todev <viktortodev@gmail.com>
2019-12-01 20:07:28 +0100

    heroku deploy

--- src/app/survey-template/survey-template.component.ts
+++ src/app/survey-template/survey-template.component.ts
@@ -7,7 +7,11 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SurveyTemplateComponent implements OnInit {
 
-  @Input() questions: string[] = ["This is first question","This is second question","This is third question","This is fourth question","This is fifth question"];
+  @Input() questions: string[] = ["This is first question",
+    "This is second question",
+    "This is third question",
+    "This is fourth question",
+    "This is fifth question"];
   constructor() { }
 
   ngOnInit() {
