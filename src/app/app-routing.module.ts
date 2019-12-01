a003e409ee3265c969e9ed91e63da79b4c1bd53b
Fico <spfilip@gmail.com>
2019-12-01 18:59:16 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateSurveyComponent } from './create-survey/create-survey.component';
+import { ReportsComponent } from './reports/reports.component';
+import { RecipientsComponent } from './recipients/recipients.component';
+import { SurveyTemplateComponent } from './survey-template/survey-template.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'create-survey', component: CreateSurveyComponent},
+  {path: 'reports', component: ReportsComponent},
+  {path: 'recipients', component: RecipientsComponent},
+  {path: 'survey-template', component: SurveyTemplateComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateSurveyComponent } from './create-survey/create-survey.component';
+import { ReportsComponent } from './reports/reports.component';
+import { RecipientsComponent } from './recipients/recipients.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { SurveyCreatorComponent } from './survey-creator/survey-creator.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SurveyTemplateComponent } from './survey-template/survey-template.component';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateSurveyComponent,
+    ReportsComponent,
+    RecipientsComponent,
+    SurveyCreatorComponent,
+    SurveyTemplateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule,
+    ReactiveFormsModule,
+    ChartsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
